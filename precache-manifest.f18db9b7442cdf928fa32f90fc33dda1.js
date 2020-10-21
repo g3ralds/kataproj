6efb3e684aae9ea0452ffcacd3e1542d36d715a5
Pedro Geraldes <>
2020-10-21 20:22:07 +0100

    Updates

---
+++ precache-manifest.f18db9b7442cdf928fa32f90fc33dda1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d238e423610983d122d8a1f9c0ec80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e3e9a0ceb0c6272d701",
+    "url": "/static/css/main.3b2085b2.chunk.css"
+  },
+  {
+    "revision": "1329c9f37a570f91bda7",
+    "url": "/static/js/2.057a2ca1.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.057a2ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3e9a0ceb0c6272d701",
+    "url": "/static/js/main.5c1b1642.chunk.js"
+  },
+  {
+    "revision": "1c7d472e2504bd319a8c",
+    "url": "/static/js/runtime-main.ac65f6e5.js"
+  },
+  {
+    "revision": "bb577d533a4733ac6fbc88ac420d41cc",
+    "url": "/static/media/estadio-jose-alvalade.bb577d53.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
